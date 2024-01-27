ffe4caf10a1b53feff1802b15c8fb07c59165497
Ajet22 <155797859+Ajet22@users.noreply.github.com>
2024-01-27 12:28:02 +0000

    Changes to be committed:     new file:   Week2/Higher Lower Game/Main.py 	new file:   Week2/Higher Lower Game/__pycache__/art.cpython-310.pyc 	new file:   Week2/Higher Lower Game/__pycache__/game_data.cpython-310.pyc 	new file:   Week2/Higher Lower Game/art.py 	new file:   Week2/Higher Lower Game/game_data.py

---
+++ Week2/Higher Lower Game/Main.py
@@ -0,0 +1,45 @@
+from game_data import data
+from art import logo,vs
+import random
+
+def get_random_account():
+  return random.choice(data)
+Score = 0  
+game_end = False
+random_number = {}
+random_number[0] = get_random_account()
+new_values = random_number[0]
+def compare_function(num1,num2):
+    global Score
+    if num1 > num2:
+      Score+=1
+      return 1
+    else:
+       return 0
+def game_on():
+  global game_end, random_number, new_values
+  while not game_end:
+    random_number[0] = new_values
+    random_number[1] = get_random_account()
+    if random_number[0] != random_number[1]:
+      print(f'\ncompare A: {random_number[0]["name"]}, {random_number[0]["description"]}, lives in {random_number[0]["country"]}\n\n {vs} \nwith B: {random_number[1]["name"]}, {random_number[1]["description"]}, lives in {random_number[1]["country"]}\n')
+    
+      user_input = input("\nWho has the highest followers \"A\" or \"B\": ").lower()
+      if user_input == "a":
+        cmp = compare_function(random_number[0]["follower_count"], random_number[1]["follower_count"])
+      elif user_input == "b":
+        cmp = compare_function(random_number[1]["follower_count"], random_number[0]["follower_count"])
+      else:
+        print("Please enter valid input\n")
+        game_end = True 
+      if cmp == 1:
+         new_values = random_number[1]
+      else:
+         print(f"You got {Score} correct! and your score is {Score}\n")
+         game_end = True
+    else:
+        game_end = True
+  
+while input("\nWanna Play a Higher - Lower Game, \"Y\" or \"N\": ").lower()=="y":
+  print(logo)
+  game_on()
